e01b4a2b4539922cbdbcd4a8b908ebf6cc5c055d
Dzmitriy Siomchanka <dzmitriy.siomchanka@innowise.com>
2025-04-26 15:36:00 +0300

    feat: add deploy folder

--- src/js/index.js
+++ src/js/index.js
@@ -31,41 +31,41 @@ document.addEventListener('DOMContentLoaded', () => {
     progressBar.style.height = `${progress}%`;
   }
 
+  function animateCards() {
+    const cards = document.querySelectorAll('.card');
+    const triggerCard = window.innerHeight * 0.8;
+    const labels = document.querySelectorAll('.label');
+    const triggerLabel = window.innerHeight / 2;
+  
+    if(labels.length > 0) {
+      labels.forEach(label => {
+        const labelTop = label.getBoundingClientRect().top;
+  
+        if (labelTop < triggerLabel) {
+          label.classList.add('filled');
+        } else {
+          label.classList.remove('filled');
+        }
+      });
+    }
+  
+    if(cards.length > 0) {
+      cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        
+        if (cardTop < triggerCard) {
+          card.classList.add('visible');
+        } else {
+          card.classList.remove('visible');
+        }
+      });
+    }
+  }
+  
+  window.addEventListener('load', animateCards);
+  window.addEventListener('scroll', animateCards);
+
   window.addEventListener('scroll', updateProgressBar);
   window.addEventListener('resize', updateProgressBar);
   updateProgressBar();
 });
-
-function animateCards() {
-  const cards = document.querySelectorAll('.card');
-  const triggerCard = window.innerHeight * 0.8;
-  const labels = document.querySelectorAll('.label');
-  const triggerLabel = window.innerHeight / 2;
-
-  if(labels.length > 0) {
-    labels.forEach(label => {
-      const labelTop = label.getBoundingClientRect().top;
-
-      if (labelTop < triggerLabel) {
-        label.classList.add('filled');
-      } else {
-        label.classList.remove('filled');
-      }
-    });
-  }
-
-  if(cards.length > 0) {
-    cards.forEach(card => {
-      const cardTop = card.getBoundingClientRect().top;
-      
-      if (cardTop < triggerCard) {
-        card.classList.add('visible');
-      } else {
-        card.classList.remove('visible');
-      }
-    });
-  }
-}
-
-window.addEventListener('load', animateCards);
-window.addEventListener('scroll', animateCards);
